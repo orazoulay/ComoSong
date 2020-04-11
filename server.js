af8df85cf69da303bad3200784aa1f9be6d73d03
bar-DY <bar@dynamicyield.com>
2020-04-11 19:35:42 +0300

    Added sequalize and files merge to the project

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-
 const http = require('http');
 const app = require('./app');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const sequalize = require('./db/sequalize');
+var ffmpeg = require('fluent-ffmpeg');
 
 
 
@@ -12,21 +13,46 @@ const port = process.env.PORT || 3001;
 //
 const server = http.createServer(app) ;
 
+
+function addFileToDb(){
+   const values = {
+        userId: 'BarTheKing3',
+        password: '1234567654rew2',
+        name: 'Bar Azoulay'
+    }; // You need to add the file as value here
+    sequalize.users.findOne({ where: {
+        userId: values.userId,
+        } })
+        .then(function(obj) {
+            // update
+            if(obj)
+                obj.update(values);
+            else 
+                sequalize.users.create(values);
+        });
+    sequalize.users.close;
+}
+// 
+async function setFiles() {
+        await ffmpeg('./resources/nick.mp4')
+            .input('./resources/nick2.mp4')
+            .on('error', function(err) {
+                console.log('An error occurred: ' + err.message);
+                reject('An error occurred: '+ err.message);
+            })
+            .mergeToFile('./resources/final.mp4')
+            .on('end', function () {
+                console.log("merged");
+                addFileToDb();
+            });
+}
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use((req,res,next)=>{
-    // res.setHeader('Content-type', 'application/json');
-    // res.setHeader('Access-Control-Allow-Origin', "*");
-    // res.writeHead(200);//status code HTTP 200 / Ok
-    //
-    // let dataObj = {"id":123, "name":"Bob","email":"bob@work.net"};
-    // let data = JSON.stringify(dataObj);
-    //
+    setFiles();
     res.end();
-
-
 });
 
-server.listen(port);
-    // console.log('Listening on port 4444
+server.listen(port, () => console.log(`Listen to port ${port}`));
