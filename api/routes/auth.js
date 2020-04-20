c2325ed76168e5e1941645f3e3643ed4c69af9aa
or azoulay <orazo123@gmail.com>
2020-04-20 11:27:31 +0300

    last commit

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -50,26 +50,26 @@ router.post('/login', async (req, res) => {
     User.findOne({username: req.body.username, password: req.body.password}).then(user => {
         if (user) {
             ///if user have song waiting on the list
-            if(user.openSong!==null){
+            if (user.openSong !== null) {
                 User.findOne({uid: user.sendSongId}).then(senderUser => {
-                   if(senderUser) {
-                       user.sendSongName = senderUser._doc.name
-                       return res.status(200).json({
-                           request: 'login',
-                           status: 1,
-                           data: {
-                               user: user
-                           }
-                       });
-                   }else{
-                       return res.status(200).json({
-                           request: 'login',
-                           status: 1,
-                           data: {
-                               user: user
-                           }
-                       });
-                   }
+                    if (senderUser) {
+                        user.sendSongName = senderUser._doc.name
+                        return res.status(200).json({
+                            request: 'login',
+                            status: 1,
+                            data: {
+                                user: user
+                            }
+                        });
+                    } else {
+                        return res.status(200).json({
+                            request: 'login',
+                            status: 1,
+                            data: {
+                                user: user
+                            }
+                        });
+                    }
                 });
             }
 
@@ -95,20 +95,28 @@ async function update(param, newDataQuery, res) {
         //     user: doc._doc
         // });
     });
+
+
 }
 
 router.post('/updateUser', async (req, res) => {
     const usersId = req.body.usersId.split('|');
-    const jsons=  [{songSubtitle: {lines: {number: "1",start: "0:00",end: "00:04:500",words:"שבעים שנה במכונית"}}},
-        {songSubtitle: {lines: {number: "2",start: "00:05",end: "00:08:00",words:"אני נוסע ומביט"}}},
-        {songSubtitle: {lines: {number: "3",start: "00:08",end: "00:11:500",words:"על מה ומה נהיה"}}}];
+    const jsons = [{
+        songSubtitle: {
+            lines: [{number: "1", start: "0:00", end: "00:04:500", words: "שבעים שנה במכונית"},
+                {number: "2", start: "00:05", end: "00:08:00", words: "אני נוסע ומביט"},
+                {number: "3", start: "00:08", end: "00:11:500", words: "על מה ומה נהיה"}]
+        }
+    },
+        {songSubtitle: {lines: {number: "2", start: "00:05", end: "00:08:00", words: "אני נוסע ומביט"}}},
+        {songSubtitle: {lines: {number: "3", start: "00:08", end: "00:11:500", words: "על מה ומה נהיה"}}}];
 
     // Remove the last slot in the array which is an empty slot
     usersId.pop();
-    for (var i = 0; i < usersId.length; i++){
+    for (var i = 0; i < usersId.length; i++) {
         //send the song name and the sender id to the other id's that in the songProcess
 
-        await update({uid: usersId[i]}, {openSong: req.body.openSong,sendSongId: req.body.uid}, res);
+        await update({uid: usersId[i]}, {openSong: req.body.openSong, sendSongId: req.body.uid}, res);
         await update({uid: usersId[i]}, jsons[i], res);
         await sleep(1000);
 
@@ -116,6 +124,18 @@ router.post('/updateUser', async (req, res) => {
     }
 
     await update({username: req.body.username}, {usersId: usersId}, res);
+
+  await User.findOne({username: req.body.username}).then(senderUser => {
+        if (senderUser) {
+            return res.status(200).json({
+                request: 'updateUser',
+                status: 1,
+                data: {
+                    user: senderUser
+                }
+            });
+        }
+    });
 });
 
 
